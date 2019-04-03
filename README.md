a3a1c0a243b474341b2e7490f377ec4bf53204fe
Chris Mutel <cmutel@gmail.com>
2019-04-03 16:56:45 +0200

    Switch to a simpler template system

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # `bonsai.uno` Webpage
 
-This is a redevelopment of the [bonsai.uno](https://bonsai.uno/) webpage using Github pages and Jekyll](https://jekyllrb.com/).
+This is a redevelopment of the [bonsai.uno](https://bonsai.uno/) webpage using [staticjinja](https://github.com/Ceasar/staticjinja) to render and Github pages to serve.
+
+See it in action here: https://bonsamurais.github.io/bonsai.uno/
+
+## Rerendering
+
+Install `staticjinja` using pip in a new virtualenv, then run `staticjinja build` in this directory.
---
+++ assets/js/scripts.js
@@ -0,0 +1,290 @@
+/*
+ * Bones Scripts File
+ * Author: Eddie Machado
+ *
+ * This file should contain any js scripts you want to add to the site.
+ * Instead of calling it in the header or throwing it inside wp_head()
+ * this file will be called automatically in the footer so as not to
+ * slow the page load.
+ *
+ * There are a lot of example functions and tools in here. If you don't
+ * need any of it, just remove it. They are meant to be helpers and are
+ * not required. It's your world baby, you can do whatever you want.
+*/
+
+
+/*
+ * Get Viewport Dimensions
+ * returns object with viewport dimensions to match css in width and height properties
+ * ( source: http://andylangton.co.uk/blog/development/get-viewport-size-width-and-height-javascript )
+*/
+function updateViewportDimensions() {
+	var w=window,d=document,e=d.documentElement,g=d.getElementsByTagName('body')[0],x=w.innerWidth||e.clientWidth||g.clientWidth,y=w.innerHeight||e.clientHeight||g.clientHeight;
+	return { width:x,height:y }
+}
+// setting the viewport width
+var viewport = updateViewportDimensions();
+
+
+/*
+ * Throttle Resize-triggered Events
+ * Wrap your actions in this function to throttle the frequency of firing them off, for better performance, esp. on mobile.
+ * ( source: http://stackoverflow.com/questions/2854407/javascript-jquery-window-resize-how-to-fire-after-the-resize-is-completed )
+*/
+var waitForFinalEvent = (function () {
+	var timers = {};
+	return function (callback, ms, uniqueId) {
+		if (!uniqueId) { uniqueId = "Don't call this twice without a uniqueId"; }
+		if (timers[uniqueId]) { clearTimeout (timers[uniqueId]); }
+		timers[uniqueId] = setTimeout(callback, ms);
+	};
+})();
+
+// how long to wait before deciding the resize has stopped, in ms. Around 50-100 should work ok.
+var timeToWaitForLast = 100;
+
+
+/*
+ * Here's an example so you can see how we're using the above function
+ *
+ * This is commented out so it won't work, but you can copy it and
+ * remove the comments.
+ *
+ *
+ *
+ * If we want to only do it on a certain page, we can setup checks so we do it
+ * as efficient as possible.
+ *
+ * if( typeof is_home === "undefined" ) var is_home = $('body').hasClass('home');
+ *
+ * This once checks to see if you're on the home page based on the body class
+ * We can then use that check to perform actions on the home page only
+ *
+ * When the window is resized, we perform this function
+ * $(window).resize(function () {
+ *
+ *    // if we're on the home page, we wait the set amount (in function above) then fire the function
+ *    if( is_home ) { waitForFinalEvent( function() {
+ *
+ *      // if we're above or equal to 768 fire this off
+ *      if( viewport.width >= 768 ) {
+ *        console.log('On home page and window sized to 768 width or more.');
+ *      } else {
+ *        // otherwise, let's do this instead
+ *        console.log('Not on home page, or window sized to less than 768.');
+ *      }
+ *
+ *    }, timeToWaitForLast, "your-function-identifier-string"); }
+ * });
+ *
+ * Pretty cool huh? You can create functions like this to conditionally load
+ * content and other stuff dependent on the viewport.
+ * Remember that mobile devices and javascript aren't the best of friends.
+ * Keep it light and always make sure the larger viewports are doing the heavy lifting.
+ *
+*/
+
+/*
+ * We're going to swap out the gravatars.
+ * In the functions.php file, you can see we're not loading the gravatar
+ * images on mobile to save bandwidth. Once we hit an acceptable viewport
+ * then we can swap out those images since they are located in a data attribute.
+*/
+// function loadGravatars() {
+//   // set the viewport using the function above
+//   viewport = updateViewportDimensions();
+//   // if the viewport is tablet or larger, we load in the gravatars
+//   if (viewport.width >= 768) {
+//   jQuery('.comment img[data-gravatar]').each(function(){
+//     jQuery(this).attr('src',jQuery(this).attr('data-gravatar'));
+//   });
+//   }
+// } // end function
+
+
+/*
+ * Put all your regular jQuery in here.
+*/
+jQuery(document).ready(function($) {
+
+  /*
+   * Let's fire off the gravatar function
+   * You can remove this if you don't need it
+  */
+  // loadGravatars();
+
+
+  /*
+   * CONRTIBUTE FORM
+  */
+  $('.contribute-form').each(function() {
+    var $this = $(this),
+        form = $this.find('form'),
+        one_time = $this.find('.one-time'),
+        monthly = $this.find('.monthly'),
+        yearly = $this.find('.yearly'),
+        current_type = null,
+        amounts = $this.find('[name=payment_amount]'),
+        lowest = amounts.filter('.lowest'),
+        label_other = $this.find('.label-other'),
+        other = label_other.find(':radio'),
+        other_text = label_other.find('.payment_custom'),
+        membership = $this.find('.membership'),
+        change_type = function() {
+
+          if(one_time.is(':checked')) {
+            current_type = 'one-time';
+            form.prop('action', '/contribute/donation/');
+            label_other.show();
+          } else {
+            current_type = monthly.is(':checked') ? 'monthly' : 'yearly';
+            form.prop('action', '/contribute/subscription/');
+            if(other.is(':checked')) {
+              lowest.prop('checked', 'checked');
+            }
+            label_other.hide();
+          }
+
+          change_amount();
+
+        },
+        change_amount = function() {
+          var value = 0;
+
+          if(other.is(':checked')) {
+            value = parseInt(other_text.val(), 10) * 100;
+          } else {
+            value = amounts.filter(':checked').val();
+          }
+
+          switch(current_type) {
+            case 'yearly':
+              if( value >= 15000 ) {
+                membership.show();
+              } else {
+                membership.hide();
+              }
+              break;
+            case 'monthly':
+              membership.hide();
+              // if( value >= 1000 ) {
+              //   membership.show();
+              // } else {
+              //   membership.hide();
+              // }
+              break;
+            default: // one-time
+              if( value >= 15000 ) {
+                membership.show();
+              } else {
+                membership.hide();
+              }
+          }
+        };
+
+    // Select other amount radio -> focus text
+    other.change(function() {
+      if(other.is(':checked')) {
+        other_text.focus().select();
+      }
+    });
+
+    // Change other text -> check other radio
+    other_text.focus(function() {
+      other.prop('checked', 'checked');
+    }).change(function() {
+      change_amount();
+    });
+
+    // Change type (one-time/monthly/yearly)
+    one_time.add(monthly).add(yearly).change(function() {
+      change_type();
+    });
+
+    // Change amount
+    amounts.change(function() {
+      change_amount();
+    });
+
+    // Run the JS-logic onload
+    change_type();
+
+  });
+
+
+
+
+
+  /*
+   * EQUAL HEIGHT ROWS (Challenge and vision)
+  */
+  var equal_height = {
+    init: function() {
+      var cols = $('.equal-height');
+
+      if( cols.length ) {
+
+        $(window).resize( $.throttle( 250, equal_height.reflow ) );
+
+        cols.each(function( index ) {
+          $(this).find('.row').each(function(i) {
+            equal_height.rows[i] = equal_height.rows[i] || [];
+            equal_height.rows[i][index] = $(this);
+          });
+        });
+
+        this.reflow();
+
+      }
+    },
+    rows: [],
+    reflow: function() {
+      $.each( equal_height.rows, function( index, row ) {
+        var max_height = 0;
+        $.each( row, function( i, col ) {
+          var height = col.height('auto').outerHeight();
+          if( height > max_height ) {
+            max_height = height;
+          }
+        });
+
+        $.each( row, function( i, col ) {
+          col.height(max_height);
+        });
+      });
+    }
+  }
+
+  equal_height.init();
+
+}); /* end of as page load scripts */
+
+/*
+ * jQuery throttle / debounce - v1.1 - 3/7/2010
+ * http://benalman.com/projects/jquery-throttle-debounce-plugin/
+ *
+ * Copyright (c) 2010 "Cowboy" Ben Alman
+ * Dual licensed under the MIT and GPL licenses.
+ * http://benalman.com/about/license/
+ */
+(function(b,c){var $=b.jQuery||b.Cowboy||(b.Cowboy={}),a;$.throttle=a=function(e,f,j,i){var h,d=0;if(typeof f!=="boolean"){i=j;j=f;f=c}function g(){var o=this,m=+new Date()-d,n=arguments;function l(){d=+new Date();j.apply(o,n)}function k(){h=c}if(i&&!h){l()}h&&clearTimeout(h);if(i===c&&m>e){l()}else{if(f!==true){h=setTimeout(i?k:l,i===c?e-m:e)}}}if($.guid){g.guid=j.guid=j.guid||$.guid++}return g};$.debounce=function(d,e,f){return f===c?a(d,e,false):a(d,f,e!==false)}})(this);
+
+// responsive-nav.min
+!function(a,b,c){"use strict";var d=function(d,e){var f=!!b.getComputedStyle;f||(b.getComputedStyle=function(a){return this.el=a,this.getPropertyValue=function(b){var c=/(\-([a-z]){1})/g;return"float"===b&&(b="styleFloat"),c.test(b)&&(b=b.replace(c,function(){return arguments[2].toUpperCase()})),a.currentStyle[b]?a.currentStyle[b]:null},this});var g,h,i,j,k,l,m=function(a,b,c,d){if("addEventListener"in a)try{a.addEventListener(b,c,d)}catch(e){if("object"!=typeof c||!c.handleEvent)throw e;a.addEventListener(b,function(a){c.handleEvent.call(c,a)},d)}else"attachEvent"in a&&("object"==typeof c&&c.handleEvent?a.attachEvent("on"+b,function(){c.handleEvent.call(c)}):a.attachEvent("on"+b,c))},n=function(a,b,c,d){if("removeEventListener"in a)try{a.removeEventListener(b,c,d)}catch(e){if("object"!=typeof c||!c.handleEvent)throw e;a.removeEventListener(b,function(a){c.handleEvent.call(c,a)},d)}else"detachEvent"in a&&("object"==typeof c&&c.handleEvent?a.detachEvent("on"+b,function(){c.handleEvent.call(c)}):a.detachEvent("on"+b,c))},o=function(a){if(a.children.length<1)throw new Error("The Nav container has no containing elements");for(var b=[],c=0;c<a.children.length;c++)1===a.children[c].nodeType&&b.push(a.children[c]);return b},p=function(a,b){for(var c in b)a.setAttribute(c,b[c])},q=function(a,b){0!==a.className.indexOf(b)&&(a.className+=" "+b,a.className=a.className.replace(/(^\s*)|(\s*$)/g,""))},r=function(a,b){var c=new RegExp("(\\s|^)"+b+"(\\s|$)");a.className=a.className.replace(c," ").replace(/(^\s*)|(\s*$)/g,"")},s=function(a,b,c){for(var d=0;d<a.length;d++)b.call(c,d,a[d])},t=a.createElement("style"),u=a.documentElement,v=function(b,c){var d;this.options={animate:!0,transition:284,label:"Menu",insert:"before",customToggle:"",closeOnNavClick:!1,openPos:"relative",navClass:"nav-collapse",navActiveClass:"js-nav-active",jsClass:"js",init:function(){},open:function(){},close:function(){}};for(d in c)this.options[d]=c[d];if(q(u,this.options.jsClass),this.wrapperEl=b.replace("#",""),a.getElementById(this.wrapperEl))this.wrapper=a.getElementById(this.wrapperEl);else{if(!a.querySelector(this.wrapperEl))throw new Error("The nav element you are trying to select doesn't exist");this.wrapper=a.querySelector(this.wrapperEl)}this.wrapper.inner=o(this.wrapper),h=this.options,g=this.wrapper,this._init(this)};return v.prototype={destroy:function(){this._removeStyles(),r(g,"closed"),r(g,"opened"),r(g,h.navClass),r(g,h.navClass+"-"+this.index),r(u,h.navActiveClass),g.removeAttribute("style"),g.removeAttribute("aria-hidden"),n(b,"resize",this,!1),n(a.body,"touchmove",this,!1),n(i,"touchstart",this,!1),n(i,"touchend",this,!1),n(i,"mouseup",this,!1),n(i,"keyup",this,!1),n(i,"click",this,!1),h.customToggle?i.removeAttribute("aria-hidden"):i.parentNode.removeChild(i)},toggle:function(){j===!0&&(l?this.close():this.open())},open:function(){l||(r(g,"closed"),q(g,"opened"),q(u,h.navActiveClass),q(i,"active"),g.style.position=h.openPos,p(g,{"aria-hidden":"false"}),l=!0,h.open())},close:function(){l&&(q(g,"closed"),r(g,"opened"),r(u,h.navActiveClass),r(i,"active"),p(g,{"aria-hidden":"true"}),h.animate?(j=!1,setTimeout(function(){g.style.position="absolute",j=!0},h.transition+10)):g.style.position="absolute",l=!1,h.close())},resize:function(){"none"!==b.getComputedStyle(i,null).getPropertyValue("display")?(k=!0,p(i,{"aria-hidden":"false"}),g.className.match(/(^|\s)closed(\s|$)/)&&(p(g,{"aria-hidden":"true"}),g.style.position="absolute"),this._createStyles(),this._calcHeight()):(k=!1,p(i,{"aria-hidden":"true"}),p(g,{"aria-hidden":"false"}),g.style.position=h.openPos,this._removeStyles())},handleEvent:function(a){var c=a||b.event;switch(c.type){case"touchstart":this._onTouchStart(c);break;case"touchmove":this._onTouchMove(c);break;case"touchend":case"mouseup":this._onTouchEnd(c);break;case"click":this._preventDefault(c);break;case"keyup":this._onKeyUp(c);break;case"resize":this.resize(c)}},_init:function(){this.index=c++,q(g,h.navClass),q(g,h.navClass+"-"+this.index),q(g,"closed"),j=!0,l=!1,this._closeOnNavClick(),this._createToggle(),this._transitions(),this.resize();var d=this;setTimeout(function(){d.resize()},20),m(b,"resize",this,!1),m(a.body,"touchmove",this,!1),m(i,"touchstart",this,!1),m(i,"touchend",this,!1),m(i,"mouseup",this,!1),m(i,"keyup",this,!1),m(i,"click",this,!1),h.init()},_createStyles:function(){t.parentNode||(t.type="text/css",a.getElementsByTagName("head")[0].appendChild(t))},_removeStyles:function(){t.parentNode&&t.parentNode.removeChild(t)},_createToggle:function(){if(h.customToggle){var b=h.customToggle.replace("#","");if(a.getElementById(b))i=a.getElementById(b);else{if(!a.querySelector(b))throw new Error("The custom nav toggle you are trying to select doesn't exist");i=a.querySelector(b)}}else{var c=a.createElement("a");c.innerHTML=h.label,p(c,{href:"#","class":"nav-toggle"}),"after"===h.insert?g.parentNode.insertBefore(c,g.nextSibling):g.parentNode.insertBefore(c,g),i=c}},_closeOnNavClick:function(){if(h.closeOnNavClick&&"querySelectorAll"in a){var b=g.querySelectorAll("a"),c=this;s(b,function(a){m(b[a],"click",function(){k&&c.toggle()},!1)})}},_preventDefault:function(a){a.preventDefault?(a.preventDefault(),a.stopPropagation()):a.returnValue=!1},_onTouchStart:function(b){b.stopPropagation(),"after"===h.insert&&q(a.body,"disable-pointer-events"),this.startX=b.touches[0].clientX,this.startY=b.touches[0].clientY,this.touchHasMoved=!1,n(i,"mouseup",this,!1)},_onTouchMove:function(a){(Math.abs(a.touches[0].clientX-this.startX)>10||Math.abs(a.touches[0].clientY-this.startY)>10)&&(this.touchHasMoved=!0)},_onTouchEnd:function(c){if(this._preventDefault(c),!this.touchHasMoved){if("touchend"===c.type)return this.toggle(),"after"===h.insert&&setTimeout(function(){r(a.body,"disable-pointer-events")},h.transition+300),void 0;var d=c||b.event;3!==d.which&&2!==d.button&&this.toggle()}},_onKeyUp:function(a){var c=a||b.event;13===c.keyCode&&this.toggle()},_transitions:function(){if(h.animate){var a=g.style,b="max-height "+h.transition+"ms";a.WebkitTransition=b,a.MozTransition=b,a.OTransition=b,a.transition=b}},_calcHeight:function(){for(var a=0,b=0;b<g.inner.length;b++)a+=g.inner[b].offsetHeight;var c="."+h.jsClass+" ."+h.navClass+"-"+this.index+".opened{max-height:"+a+"px !important}";t.styleSheet?t.styleSheet.cssText=c:t.innerHTML=c,c=""}},new v(d,e)};b.responsiveNav=d}(document,window,0);
+
+// define responsive-nav
+var nav = responsiveNav('.site-nav__list', {
+  label: 'Menu',
+  insert: 'before',
+  transition: 300,
+});
+
+// combined js throws error, when stripe is not loaded, prevent
+// if(typeof Stripe === 'undefined') {
+//   var Stripe = {
+//     'setPublishableKey': function() {
+//       return;
+//     }
+//   }, stripekey;
+// }
+// Stripe.setPublishableKey(stripekey);
